4861bf630a93d5f278ea64072178b185fe3cd110
sushant-49 <raajputsushant321@gmail.com>
2023-10-27 14:05:19 +0530

    details

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 
-const shopController = require("../controllers/shop");
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
